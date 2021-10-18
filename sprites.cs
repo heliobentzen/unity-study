d5d7c2c36f925b3eaab9658cdb6d92e4d6cff804
Helio Bentzen <heliofernando@gmail.com>
2021-10-18 16:30:02 -0300

    Create sprites.cs

---
+++ sprites.cs
@@ -0,0 +1,11 @@
+//Manipualndo Sprites
+
+//Permite fazer a transalação de um sprite
+transform.Translate(new Vector3(x, y, z));
+transform.Translate(new Vector3(0.1f, 0, 0));
+
+//Permite fazer a rotação de um sprite
+transform.Rotate(new Vector3(0, 0, 0.1f));
+
+//Permite alterar a escala de um sprite
+transform.localScale += new Vector(0.1f, 0.1f,0)
